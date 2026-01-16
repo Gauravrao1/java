f557dc79788f36e23ecf4cfd526ebe16203b6ba8
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-01-16 23:07:07 +0530

    Add files via upload

---
+++ IntegerSwapper.java
@@ -0,0 +1,17 @@
+//Q18-swap the two given integer
+public class java18 {
+    public static void main(String[] args) {
+//int temp=30;
+//a=b;
+//b=temp;
+
+        int a = 30;
+        int b = 40;
+         b=b-a;
+         a=b+a;
+         b=a-b;
+        System.out.println(a+","+b);
+
+
+    }
+}
