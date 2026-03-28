4ead30ab55a5c415f509c2404aabd98dad17f0ee
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-03-28 21:07:26 +0530

    Reformat IntroToMethods.java with consistent imports

--- IntroToMethods.java
+++ IntroToMethods.java
@@ -4,16 +4,12 @@ import java.util.*;
 public class IntroToMethods{
     public static void printhelloworld() { // define here class
         System.out.println("hello word");
-
     }
-
     public static void sum(int a, int b) { // function with parameter
         Scanner obj = new Scanner(System.in);
         int sum = a + b;
         System.out.println("sum is" + sum);
-
     }
-
     public static void main(String args[]) {
         printhelloworld();
         printhelloworld();
