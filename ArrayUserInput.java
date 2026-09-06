995259353420197177a9475b3efbe5d32eb680d6
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-09-06 23:40:08 +0530

    Update ArrayUserInput.java

--- ArrayUserInput.java
+++ ArrayUserInput.java
@@ -4,7 +4,7 @@
 import java.util.Scanner;
 public class ArrayUserInput {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
+        Scanner obj = new Scanner(System.in); 
         int[] arr = new int[12];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = obj.nextInt();
